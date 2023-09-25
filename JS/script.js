7cd9e8b537961f6b09353bd0f9e2456a796452ba
GiroSan <girosan86@gmail.com>
2023-09-25 15:09:22 -0300

    CSS ok

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+const previusOperationtext = document.querySelector("#previous-operation")
+const currentOperationText = document.querySelector("#current-operation")
+const buttons = document.querySelectorAll("#buttons-container button")
+
+class Calculator {
+    
+}
+
+buttons.forEach(btn) {
+    
+}
+
