0acfadf21fc87746cccd185ba7416b8155eb8b23
GiroSan <girosan86@gmail.com>
2023-09-26 08:43:05 -0300

    JS initial

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,40 @@
-const previusOperationtext = document.querySelector("#previous-operation")
+const previusOperationText = document.querySelector("#previous-operation")
 const currentOperationText = document.querySelector("#current-operation")
 const buttons = document.querySelectorAll("#buttons-container button")
 
 class Calculator {
-    
+    constructor(previusOperationText, currentOperationText){
+        this.previusOperationText = previusOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
+    }
+
+    //add digit to calculator screen 
+    addDigit(digit){
+        this.currentOperation = digit;
+        this.updateScreen()
+    }
+
+    //change values of the calculator screen
+    updateScreen(){
+        this.currentOperationText.innerText += this.currentOperation;
+    }
 }
 
-buttons.forEach(btn) {
+
+
+const calc = new Calculator(previusOperationText, currentOperationText);
+
+buttons.forEach((btn) =>{
+    btn.addEventListener("click", (e)=>{
+    const value = e.target.innerText;
+
+    if(+value >= 0 || value ==="."){
+        calc.addDigit(value);
+    }else{
+        console.log("Operação "+ value)
+    }
+ })
     
-}
+})
 
